80d59002a421fd64ac532ac374415a5bf118b2eb
Alexandra Ding <alex.e.ding@gmail.com>
2026-01-01 16:23:04 -0800

    click and zoom

--- main.js
+++ main.js
@@ -1,1 +1,137 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { InteractionManager } from "three.interactive";
+import {Group, Tween, Easing} from "@tweenjs/tween.js";
+
+
+// TODO:
+// allow camera orbit
+// add links --- raycasting
+// add different camera views for each planet
+// add a "sun" or "star" and lens flare / bloom / other lighting effects
+// add planets
+// each planet needs associated with it a camera view and a specific html tag
+
+
+function main() {
+
+    function makePlanet(radius, color, x, y, id) {
+
+        const geometry = new THREE.SphereGeometry(radius);
+        const material = new THREE.MeshToonMaterial({color});
+        const planet = new THREE.Mesh(geometry, material);
+
+        scene.add(planet);
+
+        planet.position.x = x;
+        planet.position.y = y;
+
+        planet.text_element = document.getElementById(id);
+
+        planet.addEventListener("click", (event) => {
+            event.stopPropagation();
+            console.log(`${id} was clicked, moving to ${planet.position.x}, ${planet.position.y}`);
+            controls.enabled = false;
+
+            new Tween(camera.position)
+                .to({x: planet.position.x, y: planet.position.y, z: 2})
+                .onUpdate(() => {
+                        controls.update();
+                })
+                .easing(Easing.Quadratic.InOut)
+                .group(Tweens)
+                .start();
+
+            new Tween(controls.target)
+                .to(planet.position)
+                .onUpdate(() => controls.update())
+                .easing(Easing.Quadratic.InOut)
+                .group(Tweens)
+                .start();
+
+            controls.enabled = true;
+            planet.text_element.style.display = "block";
+        });
+
+        interactionManager.add(planet);
+
+        return planet;
+    }
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render(time) {
+        time *= 0.001;
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            const width = canvas.clientWidth;
+            const height = canvas.clientHeight;
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+        }
+
+        planets.forEach((planet, ndx) => {
+            const speed = 0.1 + ndx * .01;
+            const rot = time * speed;
+            planet.rotation.x = rot;
+            planet.rotation.y = rot;
+        });
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+
+        controls.update();
+        interactionManager.update();
+        Tweens.update();
+    }
+
+    function add_light() {
+        const color = 0xFFFFFF;
+        const intensity = 10;
+        const light = new THREE.PointLight(color, intensity, 0, 1);
+        light.position.set(10, 10, 10);
+        scene.add(light);
+
+        const fill_light = new THREE.AmbientLight(0x404040, 1);
+        scene.add(fill_light);
+    }
+
+    const Tweens = new Group();
+
+    const colors = [0x341b5f, 0xec13a4, 0xf5e612, 0x6839eb, 0x5ed9f2]
+    // # purple magenta yellow
+
+    const canvas = document.querySelector("#c");
+    const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
+    camera.position.z = 20;
+    const scene = new THREE.Scene();
+
+    scene.background = new THREE.Color(colors[0]);
+
+    const interactionManager = new InteractionManager(renderer, camera, renderer.domElement);
+
+    const planets = [
+        makePlanet(1, colors[0], 0, 0, "planet1"),
+        makePlanet(1, colors[1], 5, 1, "planet2"),
+    ];
+
+    add_light();
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    requestAnimationFrame(render);
+
+}
+
+main()
